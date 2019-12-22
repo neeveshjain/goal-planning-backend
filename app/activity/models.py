5e991aa8da1a22b244e4cf13847eb072c35d3074
Greg kearns <kernzerly@me.com>
2019-12-21 19:04:03 -0800

    created activity model

--- app/activity/models.py
+++ app/activity/models.py
@@ -4,5 +4,33 @@ from django.db import models
 class Activity(models.Model):
     class Meta:
         verbose_name_plural = 'activities'
-    
+
     activity_name = models.CharField(max_length=300)
+    category_options = (
+        ('health','health'),
+        ('general','general'),
+        ('sales','sales'),
+        ('project','project'),
+        ('social','social'),
+        ('retirement','retirement'),
+        ('fulfillment','fulfillment'),
+        ('other','other'),
+    )
+    category = models.CharField(
+        choices=category_options,
+        max_length=300,
+        default=category_options[1]
+    )
+    priority_options = (
+        ('low','low'),
+        ('moderate','moderate'),
+        ('high','high'),
+    )
+    priority = models.CharField(
+        choices=priority_options,
+        max_length=300,
+        default=priority_options[0]
+    )
+
+    def __str__(self):
+        return(f"{self.activity_name} | {self.category} | {self.priority}")
