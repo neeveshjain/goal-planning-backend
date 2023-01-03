a617e9ffe3ceef8850a6959514e207351810c81d
Greg Kearns <gregkearns@Gregs-MBP.hsd1.ca.comcast.net>
2023-01-02 18:34:41 -0800

    configure jwtauthentication

--- app/main/settings.py
+++ app/main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +121,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {   
+    'DEFAULT_AUTHENTICATION_CLASSES': (   
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=480),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
--- app/main/urls.py
+++ app/main/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
+    path('api/token/verify/', TokenVerifyView.as_view()),
+     path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
+
 ]
+
