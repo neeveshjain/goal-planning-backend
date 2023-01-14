f303d3ee158ed7b8a93b7e4ef28a60eb5760463b
Greg Kearns <gregkearns@Gregs-MacBook-Pro.local>
2023-01-13 20:28:55 -0800

    add user serializers and views in progress

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import APIView
+from rest_framework import permissions
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+
+        return Response({}, status=status.HTTP_201_CREATED)
+
+
+class RetrieveUserView(APIView):
+    permisssion_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        pass
+
