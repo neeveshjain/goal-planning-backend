d687f66ef437862ad66c54a15698ca3fff93cf4c
Hiroyuki <xionghiroyuki@gmail.com>
2023-01-28 13:28:20 -0800

    chore(user): update RegisterViews & add URL patterns to app

--- app/users/views.py
+++ app/users/views.py
@@ -1,11 +1,23 @@
-from rest_framework import APIView
-from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status
+from .serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 class RegisterView(APIView):
     def post(self, request):
         data = request.data
+        first_name = data['first_name']
+        last_name = data['last_name']
+        email = data['email']
+        password = data['password']
 
-        return Response({}, status=status.HTTP_201_CREATED)
+        user = User.objects.create_user(first_name, last_name, email, password)
+        user = UserCreateSerializer(user)
+
+        return Response(user.data, status=status.HTTP_201_CREATED)
 
 
 class RetrieveUserView(APIView):
